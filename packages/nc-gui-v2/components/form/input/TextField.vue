9c57b63b27030dda5ec4571ac413160545d2fe3f
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2022-07-12 11:10:46 +0200

    feat(gui-v2): add custom text input component
    Signed-off-by: Braks <78412429+bcakmakoglu@users.noreply.github.com>

---
+++ packages/nc-gui-v2/components/form/input/TextField.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { computed } from '#imports'
+
+interface Props {
+  modelValue: string
+  id?: string
+  label?: string
+  type?: string
+  placeholder?: string
+  error?: string
+}
+
+interface Emits {
+  (event: 'update:modelValue', value: string): void
+}
+
+const { modelValue, label, id = undefined, type = 'text', placeholder, error } = defineProps<Props>()
+
+const emits = defineEmits<Emits>()
+
+const model = computed({
+  get: () => modelValue,
+  set: (value) => {
+    emits('update:modelValue', value)
+  },
+})
+</script>
+
+<template>
+  <label :for="id">
+    <span>{{ label }}</span>
+    <input :id="id" v-model="model" :type="type" :placeholder="placeholder" />
+  </label>
+</template>
